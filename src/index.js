7115b59f134d790ad6c24a16fbb3efd3bd19499c
its-kios09 <legacyitsolution@gmail.com>
2024-08-20 16:32:14 +0300

    refactor framework

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.scss";
-import App from "./App";
-
-ReactDOM.root.render(<App />, document.getElementById("root"));
